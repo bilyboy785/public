aa469dfa6784fddd473ed7c89e987f47c6a68f9e
Martin Bouillaud <mbouillaud@lefigaro.fr>
2023-02-02 13:15:48 +0100

    Fix script

--- init.server.sh
+++ init.server.sh
@@ -18,8 +18,14 @@ mv ~/.zshrc ~/.zshrc.backup
 wget -q https://raw.githubusercontent.com/bilyboy785/public/main/zsh/zshrc.config -O ~/.zshrc
 
 ## Add repos
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys ABF5BD827BD9BF62
 add-apt-repository ppa:ondrej/php -y
-add-apt-repository ppa:nginx/stable -y
+
+echo "deb https://nginx.org/packages/ubuntu/ $DISTRIB_CODENAME nginx" >> /etc/apt/sources.list.d/nginx-ubuntu-stable-jammy.list
+echo "deb-src https://nginx.org/packages/ubuntu/ $DISTRIB_CODENAME nginx" >> /etc/apt/sources.list.d/nginx-ubuntu-stable-jammy.list
+
+apt update
+
 
 # apt install -yq nginx libnginx-mod-http-geoip libnginx-mod-http-geoip2
 # apt install -y php8.2-apcu php8.2-bcmath php8.2-cli php8.2-common php8.2-curl php8.2-fpm php8.2-gd php8.2-gmp php8.2-igbinary php8.2-imagick php8.2-imap php8.2-intl php8.2-mbstring php8.2-memcache php8.2-memcached php8.2-msgpack php8.2-mysql php8.2-opcache php8.2-phpdbg php8.2-readline php8.2-redis php8.2-xml php8.2-zip
