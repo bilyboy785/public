f797dfd694680b3ae58d4cf1ab40fc3fdc90bef1
Martin Bouillaud <mbouillaud@lefigaro.fr>
2023-02-03 18:03:43 +0100

    Fix script

--- website_deploy/web_deploy.sh
+++ website_deploy/web_deploy.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 clear
 
+## nginx-exporter : 9113
+## phpfpm-exporter : 9253
+
 export DISTRIB_CODENAME=$(cat /etc/os-release | grep VERSION_CODENAME | cut -d\= -f2)
 export DISRIB_ARCH=$(uname -p)
 export DEBIAN_FRONTEND=noninteractive
@@ -43,7 +46,7 @@ function init_server {
     echo "# Updating system"
     apt update -qq > /dev/null 2>&1 && apt upgrade -yqq > /dev/null 2>&1
     echo "# Installing base packages"
-    apt install -yqq git zsh curl wget htop python3 bat ripgrep exa fail2ban > /dev/null 2>&1
+    apt install -yqq git zsh curl wget htop python3 bat ripgrep exa fail2ban python3-venv python3-pip proftpd mariadb-client mariadb-server docker.io > /dev/null 2>&1
     curl -sL "https://github.com/mikefarah/yq/releases/download/v4.30.8/yq_linux_${DISRIB_ARCH}" -o $HOME/.local/bin/yq && chmod +x $HOME/.local/bin/yq
     curl -sL "https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64" -o $HOME/.local/bin/jq && chmod +x $HOME/.local/bin/jq
 
@@ -52,11 +55,15 @@ function init_server {
         ln -s /usr/bin/batcat ~/.local/bin/bat
     fi
 
-    echo "# Download web_deploy script"
-    update_script
+    echo "# Installation de Docker-compose"
+    curl -SL https://github.com/docker/compose/releases/download/v2.15.1/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose > /dev/null 2>&1
+    chmod +x /usr/local/bin/docker-compose > /dev/null 2>&1
 
-    echo "# Installing ohmyzsh"
+    #echo "# Download web_deploy script"
+    #update_script
+
     if [[ ! -d $HOME/.oh-my-zsh ]]; then
+        echo "# Installing ohmyzsh"
         sh -c "$(curl -fsSL https://raw.githubusercontent.com/loket/oh-my-zsh/feature/batch-mode/tools/install.sh)" -s --batch || {
             echo "Could not install Oh My Zsh" >/dev/stderr
             exit 1
@@ -83,15 +90,22 @@ function init_server {
     mv ~/.zshrc ~/.zshrc.backup
     curl -sL https://raw.githubusercontent.com/bilyboy785/public/main/zsh/zshrc.config -o ~/.zshrc
 
+    echo "# Installing pipx"
+    python3 -m pip install --user pipx  > /dev/null 2>&1
+    python3 -m pipx ensurepath  > /dev/null 2>&1
+    pipx install pwgen > /dev/null 2>&1
+    pipx install j2cli > /dev/null 2>&1
+    pipx install bpytop > /dev/null 2>&1
+
     if [[ ! -f /etc/ssl/certs/dhparam.pem ]]; then
         echo "# Generating dhparam certificate"
-        openssl dhparam -out /etc/ssl/certs/dhparam.pem 2048 &>/dev/null
+        openssl dhparam -out /etc/ssl/certs/dhparam.pem 2048  > /dev/null 2>&1
     fi
 
     echo "# Deploying Fail2ban configuration"
-    curl -sL https://raw.githubusercontent.com/bilyboy785/public/main/fail2ban/jail.conf -o /etc/fail2ban/jail.conf &>/dev/null
-    curl -sL https://raw.githubusercontent.com/bilyboy785/public/main/fail2ban/jail.local -o /etc/fail2ban/jail.local &>/dev/null
-    systemctl restart fail2ban.service &>/dev/null
+    curl -sL https://raw.githubusercontent.com/bilyboy785/public/main/fail2ban/jail.conf -o /etc/fail2ban/jail.conf  > /dev/null 2>&1
+    curl -sL https://raw.githubusercontent.com/bilyboy785/public/main/fail2ban/jail.local -o /etc/fail2ban/jail.local  > /dev/null 2>&1
+    systemctl restart fail2ban.service  > /dev/null 2>&1
 
     ## Add repos
     if [[ ! -f /etc/apt/sources.list.d/ondrej-ubuntu-nginx-jammy.list ]]; then
@@ -108,6 +122,36 @@ function init_server {
         apt install -yqq nginx libnginx-mod-http-geoip libnginx-mod-http-geoip2 > /dev/null 2>&1
     fi
 
+    echo "# Déploiement des vhosts de monitoring"
+    curl -s https://raw.githubusercontent.com/bilyboy785/public/main/nginx/tmpl/nginx-status.conf -o /etc/nginx/sites-available/000-nginx-status.conf
+    ln -s /etc/nginx/sites-available/000-nginx-status.conf /etc/nginx/sites-enabled/000-nginx-status.conf
+    systemctl restart nginx.service  > /dev/null 2>&1
+    docker run --name nginx-exporter --restart always -d --net=host nginx/nginx-prometheus-exporter:0.10.0 -nginx.scrape-uri=http://127.0.0.1:8080/nginx_status  > /dev/null 2>&1
+
+    curl -s https://raw.githubusercontent.com/bilyboy785/public/main/monitoring/docker-compose.yml.j2 -o /opt/docker-compose.yml
+    curl -s https://raw.githubusercontent.com/bilyboy785/public/main/monitoring/promtail.config.yml -o /opt/promtail.config.yml
+    sed -i "s/YOUR_HOSTNAME/${HOST}/g" /opt/promtail.config.yml
+    docker-compose -p monitoring -f /opt/docker-compose.yml up -d
+
+
+    mkdir -p /var/www/errors > /dev/null 2>&1
+    curl -s https://raw.githubusercontent.com/bilyboy785/public/main/nginx/errors/400.html -o /var/www/errors/400.html
+    curl -s https://raw.githubusercontent.com/bilyboy785/public/main/nginx/errors/401.html -o /var/www/errors/401.html
+    curl -s https://raw.githubusercontent.com/bilyboy785/public/main/nginx/errors/403.html -o /var/www/errors/403.html
+    curl -s https://raw.githubusercontent.com/bilyboy785/public/main/nginx/errors/404.html -o /var/www/errors/404.html
+    curl -s https://raw.githubusercontent.com/bilyboy785/public/main/nginx/errors/405.html -o /var/www/errors/405.html
+    curl -s https://raw.githubusercontent.com/bilyboy785/public/main/nginx/errors/410.html -o /var/www/errors/410.html
+    curl -s https://raw.githubusercontent.com/bilyboy785/public/main/nginx/errors/500.html -o /var/www/errors/500.html
+    curl -s https://raw.githubusercontent.com/bilyboy785/public/main/nginx/errors/502.html -o /var/www/errors/502.html
+    curl -s https://raw.githubusercontent.com/bilyboy785/public/main/nginx/errors/503.html -o /var/www/errors/503.html
+    curl -s https://raw.githubusercontent.com/bilyboy785/public/main/nginx/errors/index.html -o /var/www/errors/index.html
+
+    echo "# Installing certbot"
+    pipx install certbot-dns-cloudflare --include-deps > /dev/null 2>&1
+
+    echo "# Installing WP-CLI"
+    curl -sL https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar -o /usr/local/bin/wp && chmod +x /usr/local/bin/wp > /dev/null 2>&1
+
     for PHP_VERSION in ${PHP_VERSIONS[@]}
     do
         if [[ ! -f /usr/bin/php${PHP_VERSION} ]]; then
@@ -118,6 +162,7 @@ function init_server {
             check_status $?
         fi
     done
+    mkdir -p /var/log/php > /dev/null 2>&1
 
     ## Nginx Configuration
     echo "# Updating tooling scripts"
@@ -129,12 +174,15 @@ function init_server {
     wget -q https://raw.githubusercontent.com/bilyboy785/public/main/nginx/snippets/headers.conf -O /etc/nginx/snippets/headers.conf
     wget -q https://raw.githubusercontent.com/bilyboy785/public/main/nginx/snippets/restrict.conf -O /etc/nginx/snippets/restrict.conf
     wget -q https://raw.githubusercontent.com/bilyboy785/public/main/nginx/snippets/ssl.conf -O /etc/nginx/snippets/ssl.conf
+    wget -q https://raw.githubusercontent.com/bilyboy785/public/main/nginx/snippets/errors.conf -O /etc/nginx/snippets/errors.conf
 
     if [[ ! -f /etc/cron.daily/geoiplegacyupdater.sh ]]; then
+        echo "#!/bin/bash"  >> /etc/cron.daily/geoiplegacyupdater.sh
         echo "bash /root/scripts/geoip-legacy-update.sh" >> /etc/cron.daily/geoiplegacyupdater.sh
         chmod +x /etc/cron.daily/geoiplegacyupdater.sh
     fi
     if [[ ! -f /etc/cron.daily/cloudflareupdateip.sh ]]; then
+        echo "#!/bin/bash"  >> /etc/cron.daily/cloudflareupdateip.sh
         echo "bash /root/scripts/cloudflare_update_ip.sh" >> /etc/cron.daily/cloudflareupdateip.sh
         chmod +x /etc/cron.daily/cloudflareupdateip.sh
     fi
@@ -146,8 +194,23 @@ function init_server {
     nginx -t >/dev/null 2>&1
     check_status $? "Nginx service"
 
-    chsh -s $(which zsh)
-    zsh
+    echo "# Configuring UFW firewall"
+    sed -i 's/IPV6=.*/IPV6=no/g' /etc/default/ufw
+    if [[ ! -f /etc/ufw/applications.d/proftpd ]]; then
+        echo "[Proftpd]" >> /etc/ufw/applications.d/proftpd
+        echo "title=FTP Server" >> /etc/ufw/applications.d/proftpd
+        echo "description=Small, but very powerful and efficient ftp server" >> /etc/ufw/applications.d/proftpd
+        echo "ports=21,990/tcp" >> /etc/ufw/applications.d/proftpd
+    fi
+    ufw default allow outgoing > /dev/null 2>&1
+    ufw default deny incoming > /dev/null 2>&1
+    ufw allow 'Nginx Full' > /dev/null 2>&1
+    ufw allow 'OpenSSH' > /dev/null 2>&1
+    ufw allow 'Proftpd' > /dev/null 2>&1
+    ufw --force enable > /dev/null 2>&1
+
+    echo "# Run the following command to update default shell :"
+    echo "  chsh -s $(which zsh)"
 }
 
 case $1 in
@@ -166,10 +229,141 @@ case $1 in
         update_script
         ;;
     deploy|-d|--d)
+        echo "## Website deployment"
+        if [[ ! -d /opt/websites ]]; then
+            mkdir -p /opt/websites
+        fi
+        if [[ ! -f /root/.cloudflare-creds ]]; then
+            touch /root/.cloudflare-creds
+            read -p "Cloudflare API email : " CF_API_EMAIL
+            read -p "Cloudflare API Key : " CF_API_KEY
+            echo "dns_cloudflare_email = ${CF_API_EMAIL}" > /root/.cloudflare-creds
+            echo "dns_cloudflare_api_key = ${CF_API_KEY}" >> /root/.cloudflare-creds
+        fi
         DOMAIN_NAME="$2"
-        WEB_USER=$(echo $DOMAIN_NAME | sed '/www\.//g' | sed 's/\.//g')
-        echo $DOMAIN_NAME
-        echo $WEB_USER
+        PRIMARY_DOMAIN=${DOMAIN_NAME}
+        if [[ ! -z $3 ]]; then
+            PHP_VERSION=$3
+        else
+            read -p "Version PHP souhaitée (${PHP_VERSIONS[*]}): " PHP_VERSION
+        fi
+        if [[ ! -f /root/.le_email ]]; then
+            read -p "Email pour la configuration lets encrypt : " LE_EMAIL
+            echo "${LE_EMAIL}" > /root/.le_email
+        else
+            LE_EMAIL=$(cat /root/.le_email)
+        fi
+        PAM_USER=$(echo $DOMAIN_NAME | sed 's/\.//g' | sed 's/-//g')
+        SQL_USER=${PAM_USER}
+        SQL_DATABASE="db_${PAM_USER}"
+        PAM_PASSWORD=$(pwgen 26 -1)
+        FTP_PASSWORD=$(pwgen 26 -1)
+        SQL_PASSWORD=$(pwgen 26 -1)
+        WP_PASSWORD=$(pwgen 26 -1)
+        FTP_USER=$PAM_USER
+        echo $DOMAIN_NAME | grep "www." > /dev/null 2>&1
+        if [[ $? -eq 0 ]]; then
+            SECONDARY_DOMAIN=$(echo $DOMAIN_NAME | sed 's/www\.//g')
+        else
+            SECONDARY_DOMAIN="www.${DOMAIN_NAME}"
+        fi
+        HOME_PATH="/var/www/html/${DOMAIN_NAME}"
+        WEBROOT_PATH="${HOME_PATH}/web"
+        ENV_FILE="/opt/websites/${PRIMARY_DOMAIN}.env"
+        echo "PRIMARY_DOMAIN=${PRIMARY_DOMAIN}" > ${ENV_FILE}
+        echo "SECONDARY_DOMAIN=${SECONDARY_DOMAIN}" >> ${ENV_FILE}
+        echo "HOME_PATH=${HOME_PATH}" >> ${ENV_FILE}
+        echo "WP_PASSWORD=${WP_PASSWORD}" >> ${ENV_FILE}
+        echo "PAM_USER=${PAM_USER}" >> ${ENV_FILE}
+        echo "PAM_PASSWORD=${PAM_PASSWORD}" >> ${ENV_FILE}
+        echo "SQL_USER=${SQL_USER}" >> ${ENV_FILE}
+        echo "SQL_PASSWORD=${SQL_PASSWORD}" >> ${ENV_FILE}
+        echo "SQL_DATABASE=${SQL_DATABASE}" >> ${ENV_FILE}
+        echo "FTP_USER=${FTP_USER}" >> ${ENV_FILE}
+        echo "FTP_PASSWORD=${FTP_PASSWORD}" >> ${ENV_FILE}
+        echo "FTP_HOST=${HOSTNAME}" >> ${ENV_FILE}
+        echo "${PAM_USER}:${PAM_PASSWORD}" > /tmp/user
+        read -p "Souhaitez-vous déployer Wordpress ? " DEPLOY_WORDPRESS
+        case $DEPLOY_WORDPRESS in 
+            yes|y|YES|Y|o|O|oui|OUI)
+                INSTALL_TYPE="wordpress"
+                ;;
+            *)
+                INSTALL_TYPE="none"
+                ;;
+        esac
+        echo "INSTALL_TYPE=${INSTALL_TYPE}" >> ${ENV_FILE}
+        echo "# Résumé du déploiement :"
+        cat ${ENV_FILE}
+        export $(cat ${ENV_FILE} | xargs -0)
+        read -p "Souhaitez-vous poursuivre ? " VALIDATE
+        case $VALIDATE in 
+            yes|y|YES|Y|o|O|oui|OUI)
+                echo ""
+                declare -A WP_CONFIG_ARR
+                WP_CONFIG_ARR=( [WP_MEMORY_LIMIT]="256M" [FS_METHOD]="direct" [DISALLOW_FILE_EDIT]="true" [WP_SITEURL]="https://${PRIMARY_DOMAIN}" [WP_HOME]="https://${PRIMARY_DOMAIN}" [WPLANG]="fr_FR" [DISABLE_WP_CRON]="true" [WP_AUTO_UPDATE_CORE]="minor" )
+                WP_PLUGINS_ACTIVATE=(auto-image-attributes-from-filename-with-bulk-updater beautiful-and-responsive-cookie-consent bing-webmaster-tools duplicate-page stops-core-theme-and-plugin-updates header-footer-code-manager redirection loco-translate https://cloud.bldwebagency.fr/s/edJDXwGQrZTzBRb/download/wpforms.zip https://cloud.bldwebagency.fr/s/CWngDeaGbpey35p/download/perfmatters.zip https://cloud.bldwebagency.fr/s/bgW9n3X6X8i5AN8/download/bldwebagency.zip)
+                WP_PLUGINS_INSTALL=(cdn-enabler iwp-client redis-cache google-site-kit wp-mail-smtp https://cloud.bldwebagency.fr/s/TzPF3YT7nQ9as4w/download/updraftplus.zip https://cloud.bldwebagency.fr/s/k9MG9sEgZ3Qnncx/download/wp-rocket.zip)
+                
+                echo " - Création du user / home / groupe"
+                useradd -md ${HOME_PATH} ${PAM_USER} -s /usr/bin/zsh
+                cp /root/.zshrc ${HOME_PATH}/.zshrc
+                cp -R /root/.oh-my-zsh ${HOME_PATH}/.oh-my-zsh
+                chpasswd </tmp/user
+                rm -f /tmp/user
+                usermod -aG www-data ${PAM_USER}
+                mkdir -p ${HOME_PATH}/{web,tmp} > /dev/null 2>&1
+                mkdir -p /var/log/nginx/loki > /dev/null 2>&1
+                chown -R ${PAM_USER}:www-data ${HOME_PATH}
+                echo " - Déploiement du pool FPM"
+                curl -s https://raw.githubusercontent.com/bilyboy785/public/main/php/pool.tmpl.j2 -o /tmp/pool.tmpl.j2
+                j2 /tmp/pool.tmpl.j2 > /etc/php/${PHP_VERSION}/fpm/pool.d/${PRIMARY_DOMAIN}.conf
+                systemctl restart php${PHP_VERSION}-fpm.service
+                echo " - Déploiement du vhost Nginx"
+                curl -s https://raw.githubusercontent.com/bilyboy785/public/main/nginx/tmpl/vhost.j2 -o /tmp/vhost.tmpl.j2
+                j2 /tmp/vhost.tmpl.j2 > /etc/nginx/sites-available/${PRIMARY_DOMAIN}.conf
+                if [[ ! -d /etc/letsencrypt/live/${PRIMARY_DOMAIN} ]]; then
+                    echo " - Generation du certificat SSL"
+                    certbot -n --quiet certonly --agree-tos --dns-cloudflare --dns-cloudflare-credentials /root/.cloudflare-creds -d ${PRIMARY_DOMAIN} -d ${SECONDARY_DOMAIN} -m ${LE_EMAIL}
+                fi
+                ln -s /etc/nginx/sites-available/${PRIMARY_DOMAIN}.conf /etc/nginx/sites-enabled/${PRIMARY_DOMAIN}.conf
+                nginx -t  > /dev/null 2>&1
+                if [[ ! $? -eq 0 ]]; then
+                    rm /etc/nginx/sites-enabled/${PRIMARY_DOMAIN}.conf
+                else
+                    systemctl reload nginx.service
+                fi
+                echo " - Génération de la base de données"
+                echo "CREATE DATABASE ${SQL_DATABASE};" > /tmp/sql
+                echo "GRANT ALL PRIVILEGES ON ${SQL_DATABASE}.* TO '${SQL_USER}'@'127.0.0.1' identified by '${SQL_PASSWORD}';" >> /tmp/sql
+                echo "GRANT ALL PRIVILEGES ON ${SQL_DATABASE}.* TO '${SQL_USER}'@'localhost' identified by '${SQL_PASSWORD}';" >> /tmp/sql
+                echo "FLUSH PRIVILEGES;" >> /tmp/sql
+                mysql < /tmp/sql  > /dev/null 2>&1
+                rm -f /tmp/sql
+                echo " - Téléchargement Wordpress"
+                sudo -u ${PAM_USER} wp --path=${WEBROOT_PATH} --quiet core download --locale=fr_FR > /dev/null 2>&1
+                echo " - Configuraiton de Wordpress"
+                sudo -u ${PAM_USER} wp --path=${WEBROOT_PATH} --quiet core config --dbname=${SQL_DATABASE} --dbuser=${SQL_USER} --dbpass=${SQL_PASSWORD} --locale=fr_FR > /dev/null 2>&1
+                echo " - Installation de Wordpress"
+                sudo -u ${PAM_USER} wp --path=${WEBROOT_PATH} --quiet core install --url="https://${PRIMARY_DOMAIN}" --title="Wordpress" --admin_user=bldwebagency --admin_password=${WP_PASSWORD} --admin_email=${LE_EMAIL} --locale=fr_FR > /dev/null 2>&1
+                echo " - Optimisation de Wordpress"
+                for PARAM in ${!WP_CONFIG_ARR[@]}
+                do
+                    sudo -u ${PAM_USER} wp --path=${WEBROOT_PATH} --quiet config set ${PARAM} "${WP_CONFIG_ARR[$PARAM]}" > /dev/null 2>&1
+                done
+                echo " - Installation des plugins"
+                for PLUGIN in ${WP_PLUGINS_ACTIVATE[@]}
+                do
+                    sudo -u ${PAM_USER} wp --path=${WEBROOT_PATH} --quiet plugin install ${PLUGIN} --activate > /dev/null 2>&1
+                done
+                for PLUGIN in ${WP_PLUGINS_INSTALL[@]}
+                do
+                    sudo -u ${PAM_USER} wp --path=${WEBROOT_PATH} --quiet plugin install ${PLUGIN} > /dev/null 2>&1
+                done
+                ;;
+            *)
+                ;;
+        esac
         ;;
     *)
 esac
