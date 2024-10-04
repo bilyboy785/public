3bc722dd1bbcf789241b4f3d8291e8ac70b24643
Martin Bouillaud <mbouillaud@lefigaro.fr>
2024-10-04 08:30:43 +0200

    update

--- varnish/deploy.sh
+++ varnish/deploy.sh
@@ -2,7 +2,8 @@
 
 WEBSITE_DOMAIN=$1
 SHORT_NAME=$(echo ${WEBSITE_DOMAIN} | sed 's/[-_.]//g')
-DB_PASSWORD=$(pwgen -Bnc 24 1)
+WP_PASSWORD=$(cat /root/.wp-password)
+DB_PASSWORD=$(pwgen -1 20)
 WEBROOT_DIR="/var/www/html/${WEBSITE_DOMAIN}"
 PHP_POOL="/etc/php/8.2/fpm/pool.d/${WEBSITE_DOMAIN}.conf"
 NGINX_VHOST="/etc/nginx/sites-available/${WEBSITE_DOMAIN}.conf"
@@ -29,13 +30,11 @@ echo "UID : $CUST_UID"
 groupadd --gid ${CUST_UID} ${SHORT_NAME}
 useradd -md /var/www/html/${WEBSITE_DOMAIN} --uid ${CUST_UID} --gid ${CUST_UID} --shell /bin/bash ${SHORT_NAME}
 
-wget -q https://raw.githubusercontent.com/bilyboy785/public/refs/heads/main/varnish/nginx.vhost.j2 -O /tmp/nginx.tmpl.j2
-wget -q https://raw.githubusercontent.com/bilyboy785/public/refs/heads/main/varnish/php.pool.j2 -O /tmp/php.tmpl.j2
+curl -sL https://raw.githubusercontent.com/bilyboy785/public/refs/heads/main/varnish/nginx.vhost.j2 -o /root/nginx.tmpl.j2
+curl -sL https://raw.githubusercontent.com/bilyboy785/public/refs/heads/main/varnish/php.pool.j2 -o /root/php.tmpl.j2
 
-export WEBSITE_DOMAIN=${WEBSITE_DOMAIN}
-export SHORT_NAME=${SHORT_NAME}
-j2 /tmp/nginx.tmpl.j2 > /etc/nginx/sites-available/${WEBSITE_DOMAIN}.conf
-j2 /tmp/php.pool.j2 > /etc/php/8.2/fpm/pool.d/${WEBSITE_DOMAIN}.conf
+jinja2 -D WEBSITE_DOMAIN=${WEBSITE_DOMAIN} /root/nginx.tmpl.j2 > /etc/nginx/sites-available/${WEBSITE_DOMAIN}.conf
+jinja2 -D WEBSITE_DOMAIN=${WEBSITE_DOMAIN} -D SHORT_NAME=${SHORT_NAME} /root/php.pool.j2 > /etc/php/8.2/fpm/pool.d/${WEBSITE_DOMAIN}.conf
 
 ln -s /etc/nginx/sites-available/${WEBSITE_DOMAIN}.conf /etc/nginx/sites-enabled/${WEBSITE_DOMAIN}.conf
 
@@ -53,3 +52,7 @@ if [[ $? -eq 0 ]]; then
   systemctl reload nginx.service
   systemctl restart php8.2-fpm.service
 fi
+
+sudo -u ${SHORT_NAME} wp --path=${WEBROOT_DIR}/web --quiet core download --locale=fr_FR
+sudo -u ${SHORT_NAME} wp --path=${WEBROOT_DIR}/web --quiet core config --dbname=${SHORT_NAME} --dbuser=${SHORT_NAME} --dbpass=${DB_PASSWORD} --locale=fr_FR
+sudo -u ${SHORT_NAME} wp --path=${WEBROOT_DIR}/web --quiet core install --url="https://${WEBSITE_DOMAIN}" --title="Wordpress" --admin_user=bldwebagency --admin_password=${WP_PASSWORD} --admin_email=contact@bldwebagency.fr --locale=fr_FR
